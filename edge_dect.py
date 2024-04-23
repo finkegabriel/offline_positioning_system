9574d0e5f900451357a45ee70349a09d9e7de5b6
finkegabriel <finkegabriel@gmail.com>
2024-04-22 19:45:47 -0700

    update

---
+++ edge_dect.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+ 
+img = cv.imread('mapimage.jpeg', cv.IMREAD_GRAYSCALE)
+assert img is not None, "file could not be read, check with os.path.exists()"
+edges = cv.Canny(img,100,200)
+
+# plt.subplot(121),plt.imshow(img,cmap = 'gray')
+# plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.imshow(edges,cmap = 'gray')
+# plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+# plt.show()
+plt.savefig("map")
+plt.close()
