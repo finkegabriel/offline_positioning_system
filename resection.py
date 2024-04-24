5fe7d781c9de9ada7c9e3be23bc302e79d7663e9
finkegabriel <finkegabriel@gmail.com>
2024-04-24 15:07:22 -0700

    update

--- resection.py
+++ resection.py
@@ -1,10 +1,33 @@
 '''
-This is a program desgined to do resection on a map given its coords. N,S,E,W in lat long.
+This is a program desgined to do resection on a raster map given its coords. N,S,E,W in lat long.
 to find position
 
 In psuedo code this is the design of the system.
 
 input the lat long of the entire extent 
 
-
 '''
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import cv2 as cv
+import imutils
+
+x = [1500,3025,1500,0]
+y = [0,1137.5,2275,1137.5]
+n = ["N","E","S","W"]
+
+fig, ax = plt.subplots()
+ax.scatter(x, y)
+
+for i, txt in enumerate(n):
+    ax.annotate(txt, (x[i], y[i]))
+
+# plt.axis('off')
+img = cv.imread('mapimage.jpeg')
+
+plt.imshow(img)
+plt.savefig("fig")
+# plt.show()
+
