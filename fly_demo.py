fc32a2ed0f4f1c4e364ac406b2655f48d544c062
finkegabriel <finkegabriel@gmail.com>
2024-04-23 20:32:14 -0700

    update

--- fly_demo.py
+++ fly_demo.py
@@ -15,15 +15,15 @@ img = cv.imread('mapimage.jpeg')
 # each frame
 
 ims = []
-for i in range(100):
+for i in range(200):
     shifted = imutils.translate(img, 0, i*2.5)
     im = plt.imshow(shifted, animated=True)
     ims.append([im])
 
-fig_two.set_xlim(1000, 500)
+fig_two.set_xlim(1000, 2000)
 fig_two.set_ylim(2000,500)
 ani = animation.ArtistAnimation(fig, ims, interval=100, blit=True)
 
 ani.save('fly_over.mp4',fps=10)
 
-plt.show()
+# plt.show()
