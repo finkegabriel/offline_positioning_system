79197d3d3e95c691db7e714abbb6a4edac41a300
finkegabriel <finkegabriel@gmail.com>
2024-04-24 12:51:04 -0700

    update

--- fly_demo.py
+++ fly_demo.py
@@ -4,35 +4,31 @@ import matplotlib.animation as animation
 import cv2 as cv
 import imutils
 
-
 fig = plt.figure()
 fig_two = plt.axes()
 plt.axis('off')
-img = cv.imread('mapimage.jpeg')
+img = cv.imread('mn_2.jpeg')
 
 # ims is a list of lists, each row is a list of artists to draw in the
 # current frame; here we are just animating one artist, the image, in
 # each frame
 
 ims = []
-
 COUNTER=0
-while COUNTER<1000:
+
+while COUNTER<700:
     shifted = imutils.translate(img, 0, COUNTER*2.5)
     im = plt.imshow(shifted, animated=True)
     ims.append([im])
     COUNTER+=1
+    # print("COUNTER ",imt)
     if COUNTER == 1000:
         break
 
-    # shifted = imutils.translate(img, 0, i*2.5)
-    # im = plt.imshow(shifted, animated=True)
-    # ims.append([im])
-
 fig_two.set_xlim(1000, 2000)
 fig_two.set_ylim(2000,500)
+
 ani = animation.ArtistAnimation(fig, ims, interval=100, blit=True)
 
-ani.save('fly_over.mp4',fps=10)
-
 # plt.show()
+ani.save('fly_over_mn.mp4',fps=10)
---
+++ resection.py
@@ -0,0 +1,10 @@
+'''
+This is a program desgined to do resection on a map given its coords. N,S,E,W in lat long.
+to find position
+
+In psuedo code this is the design of the system.
+
+input the lat long of the entire extent 
+
+
+'''
