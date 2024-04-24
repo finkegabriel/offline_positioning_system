e49c4a026b9fd4637a3450d4101cd59100b72ec2
finkegabriel <finkegabriel@gmail.com>
2024-04-23 17:35:56 -0700

    zoom

--- fly_demo.py
+++ fly_demo.py
@@ -4,7 +4,9 @@ import matplotlib.animation as animation
 import cv2 as cv
 import imutils
 
+
 fig = plt.figure()
+fig_two = plt.axes()
 plt.axis('off')
 img = cv.imread('mapimage.jpeg')
 
@@ -13,13 +15,15 @@ img = cv.imread('mapimage.jpeg')
 # each frame
 
 ims = []
-for i in range(350):
-    shifted = imutils.translate(img, 0, i)
+for i in range(100):
+    shifted = imutils.translate(img, 0, i*2.5)
     im = plt.imshow(shifted, animated=True)
     ims.append([im])
 
+fig_two.set_xlim(1000, 500)
+fig_two.set_ylim(2000,500)
 ani = animation.ArtistAnimation(fig, ims, interval=100, blit=True)
 
 ani.save('fly_over.mp4',fps=10)
 
-# plt.show()
+plt.show()
