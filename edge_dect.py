1a1dcd40c9676b27f3ce28a13a8c5f21c4549081
finkegabriel <finkegabriel@gmail.com>
2024-11-18 19:48:49 -0700

    edge convert

--- edge_dect.py
+++ edge_dect.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
  
-img = cv.imread('mapimage.jpeg', cv.IMREAD_GRAYSCALE)
+img = cv.imread('map_3.png', cv.IMREAD_GRAYSCALE)
 assert img is not None, "file could not be read, check with os.path.exists()"
 edges = cv.Canny(img,100,450)
 
@@ -10,5 +10,5 @@ plt.imshow(edges,cmap = 'gray')
 
 
 plt.axis('off')
-plt.savefig("mapimage_output",transparent = True, bbox_inches = 'tight', pad_inches = 0)
+plt.savefig("map_3_output",transparent = True, bbox_inches = 'tight', pad_inches = 0)
 plt.close()
